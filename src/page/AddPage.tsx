36e7d7d28429ecd6c913711f97dc9d35afe087cb
ralfs <ralfs.ulmanis@gmail.com>
2023-02-28 21:46:19 +0200

    Initiated building the page look

---
+++ src/page/AddPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function AddPage() {
+  return (
+    <div>
+a
+    </div>
+  );
+}
+
+export default AddPage;
---
+++ src/page/ListPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../css/productList.scss';
+
+function ListPage() {
+  return (
+    <div className='header'>
+      <div className='horizontalBox'>
+
+      </div>
+
+      <div className='line'></div>
+    </div>
+  );
+}
+
+export default ListPage;
