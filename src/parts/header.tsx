0bdfb9f45826b3009762f2dae74832360d2eec1e
ralfs <ralfs.ulmanis@gmail.com>
2023-03-02 20:24:57 +0200

    Adjustments

--- src/parts/header.tsx
+++ src/parts/header.tsx
@@ -30,8 +30,8 @@ function AddInfo() {
             <button>
                 SAVE
             </button>
-            
-            <button>
+
+            <button onClick={ChangeRoute("/")}>
                 CANCEL
             </button>
         </div>
