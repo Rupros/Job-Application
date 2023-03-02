06ae640986cba5a476471b193175026494fcafb3
ralfs <ralfs.ulmanis@gmail.com>
2023-03-02 21:41:45 +0200

    Added input fields on product add page

--- src/page/AddPage.tsx
+++ src/page/AddPage.tsx
@@ -7,8 +7,30 @@ function AddPage() {
     <>
       <AddHeader/>
 
-      <div>
+      <div className='center'>
+        <div className='verticalBox'>
+          <form id='#product_form'>
+            <div className='inputFields'>
+              <div>
+                <label>SKU</label>
+                <input type="text" id='#sku'/>
+              </div>
+
+              <div>
+                <label>Name</label>
+                <input type="text" id='#name'/>
+              </div>
+
+              <div>
+                <label>Price ($)</label>
+                <input type="text" id='#price'/>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
+
+
     </>
   );
 }
